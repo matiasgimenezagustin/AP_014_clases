1f33c60855fc0dd53cec081df59273ca4191f66d
matu.dev <matu.a.gimenez2002@gmail.com>
2023-11-13 11:34:43 -0300

    clase 5

--- react-clase-3 a 7/react-workspace/src/App.jsx
+++ react-clase-3 a 7/react-workspace/src/App.jsx
@@ -7,14 +7,49 @@ function App() {
   
   const saludar = () => 'hola'
   let nombre = 'jacinto'
+  const comprar = () =>{
+    alert('comprado')
+  }
 
+  const archivos = [
+    {
+      nombre: 'hola',
+      extension: 'txt',
+      peso: '2kb',
+      fechaDeCreacion: '2022/03/08',
+      tipo: 'archivo'
+    },
+    {
+      nombre: 'index',
+      extension: 'html',
+      peso: '50kb',
+      fechaDeCreacion: '2023/04/01',
+      tipo: 'archivo'
+    },
+  ]
   return (  
     <div>
       <Header/>
       <div className='product-container'>
         
-        <Product nombre='tv samsung 40"' precio={450} estaComprado={true} esFavorito={false}/>
-        <Product estaComprado={false} esFavorito={true}/>
+        <Product nombre='tv samsung 40"' precio={450} estaComprado={true} esFavorito={false} comprar={comprar}/>
+        <Product estaComprado={false} esFavorito={true} comprar={comprar}/>
+
+
+      </div>
+      <div>
+        {
+          archivos.map(
+            (archivo) =>(
+              <div className='item'>
+                <h2>{archivo.nombre + '.' + archivo.extension}</h2>
+                <span>Peso: {archivo.peso}</span>
+                <span>Fecha de creacion: {archivo.fechaDeCreacion}</span>
+                <hr/>
+              </div>
+            )
+          )
+        }
       </div>
     </div>
     
@@ -23,7 +58,7 @@ function App() {
 
 export default App
 
-
+/* Principio DRY dont repeat yourself */
 
 /* 
 Alumnos mayor => class
@@ -33,3 +68,36 @@ Alumnos mayor => class
 
 
 /* Debemos crear una carta que muestre un nombre un dni y debe ser de 200 w x 100 h y un fondo de color celeste*/
+
+
+
+/* 
+Estructura a reperir
+<div className='item'>
+          <h2>Hola.txt</h2>
+          <span>Peso: 3kb</span>
+          <span>Fecha de creacion: 2023/08/09</span>
+          <hr/>
+        </div>
+*/
+
+
+/* 
+Vamos a crear un array que sea representativo de una lista de cosas para hacer el finde que viene
+
+estructura: 
+
+
+nombre de la tarea
+
+descripcion de la tarea
+
+tiempo estimado
+
+finalizar antes de:
+
+fecha de creacion
+
+
+Luego de esto mapear a dicha lista de tareas para representarlas en la pagina
+*/
--- react-clase-3 a 7/react-workspace/src/components/Product/Product.jsx
+++ react-clase-3 a 7/react-workspace/src/components/Product/Product.jsx
@@ -6,15 +6,20 @@ const Product = (propiedades) =>{
     let precio = propiedades.precio ? propiedades.precio : '0'
     precio = Number(precio).toFixed(2)
     let iva = (precio * 0.21).toFixed(2)
+
+  /* Declaramos la funcion comprar */
+    
+
+
     return(
       <div className='producto'>
-        <h2>{nombre}</h2>
+        <h2 onMouseOver={() => console.log('pasaste por encima')}>{nombre}</h2>
         <span>Precio: ${precio}</span>
         <span>Iva: ${iva}</span>
         {
             propiedades.estaComprado
             ?<button className='btn btn-bought'>Comprado</button>
-            :<button className='btn btn-buy'>Comprar</button>
+            :<button className='btn btn-buy' onClick={propiedades.comprar} >Comprar</button>
         }
         {
             propiedades.esFavorito
